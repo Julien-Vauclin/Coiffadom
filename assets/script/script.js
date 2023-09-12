32639fa18cdbd10fbf04752fd8e618e0d973feb4
67ujuj <vauclinjulien@gmail.com>
2023-09-12 16:11:17 +0200

    script.js rangé

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,10 @@
 // VARIABLES
-var searchPopup = document.querySelector('.search-popup');
+var searchPopup = document.querySelector('.search-popup'); /* Loupe */
+var motdepasse =
+  document.getElementById('motdepasse'); /* Force du mot de passe */
+var secure = document.getElementById('secure'); /* Force du mot de passe */
 // FONCTIONS
+// LOUPE
 // Ouverture popup recherche navbar
 function openSearchPopup() {
   searchPopup.style.display = 'flex';
@@ -31,3 +35,62 @@ window.onkeydown = function (event) {
       openSearchPopup();
     }
 };
+// MOT DE PASSE
+// Affichage mot de passe
+function displayPassword() {
+  var x = document.getElementById('motdepasse');
+  if (x.type === 'password') {
+    x.type = 'text';
+  } else {
+    x.type = 'password';
+  }
+}
+// Affichage confirmation mot de passe
+function displayConfirmPassword() {
+  var x = document.getElementById('passwordConfirm');
+  if (x.type === 'password') {
+    x.type = 'text';
+  } else {
+    x.type = 'password';
+  }
+}
+// Force du mot de passe
+motdepasse.addEventListener('input', function () {
+  // Regex
+  let regexMini = new RegExp('[a-z]');
+  let regexMaj = new RegExp('[A-Z]');
+  let regexNombre = new RegExp('[0-9]');
+  let regexSpec = new RegExp('[@?!$]');
+  // Variable de sécurité
+  let security = 0;
+  // Niveau de sécurité
+  if (regexMini.test(motdepasse.value)) {
+    security++;
+  }
+  if (regexMaj.test(motdepasse.value)) {
+    security++;
+  }
+  if (regexNombre.test(motdepasse.value)) {
+    security++;
+  }
+  if (regexSpec.test(motdepasse.value)) {
+    security++;
+  }
+  if (motdepasse.value.length >= 8) {
+    security++;
+  }
+  // Changement du texte en fonction de la sécurité
+  if (security == 0) {
+    secure.innerHTML = '<p>Inexistant</p>';
+  } else if (security == 1) {
+    secure.innerHTML = '<p style="color: red">Dangereux</p>';
+  } else if (security == 2) {
+    secure.innerHTML = '<p style="color: orange">Moyen</p>';
+  } else if (security == 3) {
+    secure.innerHTML = '<p style="color: gold">Sécurisé</p>';
+  } else if (security == 4) {
+    secure.innerHTML = '<p style="color: lime">Très sécurisé</p>';
+  } else if (security == 5) {
+    secure.innerHTML = '<p style="color: deeppink">Ultra sécurisé !</p>';
+  }
+});
