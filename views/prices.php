fc56c8059fdc1a1d7dd506fa535f03b18b29afbb
67ujuj <vauclinjulien@gmail.com>
2023-11-01 13:34:55 +0100

    page tarifs ok

--- views/prices.php
+++ views/prices.php
@@ -1,66 +1,66 @@
 <?php session_start(); ?>
 <?php require_once "components/head.php" ?>
 <?php require_once "components/navbar.php" ?>
-<div class="container">
-    <table class="table table-bordered">
+<div>
+    <table class="priceGrid">
         <thead>
             <tr>
                 <th></th>
-                <th>Court</th>
-                <th>Mi-long</th>
-                <th>Long</th>
-                <th>Très long</th>
+                <th class="priceGridRow">Court</th>
+                <th class="priceGridRow">Mi-long</th>
+                <th class="priceGridRow">Long</th>
+                <th style="padding-left: 5px;">Très long</th>
             </tr>
         </thead>
         <tbody>
             <tr>
-                <th scope="row">Coupe + brushing</th>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
+                <th class="priceGridRow" scope="row">Coupe + brushing</th>
+                <td class="priceGridRow" contenteditable="true">26€</td>
+                <td class="priceGridRow" contenteditable="true">31€</td>
+                <td class="priceGridRow" contenteditable="true">35€</td>
+                <td class="priceGridRow" contenteditable="true">42€</td>
             </tr>
             <tr>
-                <th scope="row">Balayage</th>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
+                <th class="priceGridRow" scope="row">Balayage</th>
+                <td class="priceGridRow" contenteditable="true">30€</td>
+                <td class="priceGridRow" contenteditable="true">45€</td>
+                <td class="priceGridRow" contenteditable="true">52€</td>
+                <td class="priceGridRow" contenteditable="true">60€</td>
             </tr>
             <tr>
-                <th scope="row">Global</th>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
+                <th class="priceGridRow" scope="row">Mèches</th>
+                <td class="priceGridRow" contenteditable="true">35€</td>
+                <td class="priceGridRow" contenteditable="true">52€</td>
+                <td class="priceGridRow" contenteditable="true">65€</td>
+                <td class="priceGridRow" contenteditable="true">90€</td>
             </tr>
             <tr>
-                <th scope="row">Coloration</th>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
+                <th class="priceGridRow" scope="row">Coloration</th>
+                <td class="priceGridRow" contenteditable="true">30€</td>
+                <td class="priceGridRow" contenteditable="true">35€</td>
+                <td class="priceGridRow" contenteditable="true">45€</td>
+                <td class="priceGridRow" contenteditable="true">60€</td>
             </tr>
             <tr>
-                <th scope="row">Racines</th>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
+                <th class="priceGridRow" scope="row">Racines</th>
+                <td class="priceGridRow" contenteditable="true">24€</td>
+                <td class="priceGridRow" contenteditable="true">24€</td>
+                <td class="priceGridRow" contenteditable="true">29€</td>
+                <td class="priceGridRow" contenteditable="true">29€</td>
             </tr>
             <tr>
-                <th scope="row">Patine</th>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
+                <th class="priceGridRow" scope="row">Patine</th>
+                <td class="priceGridRow" contenteditable="true">12€</td>
+                <td class="priceGridRow" contenteditable="true">20€</td>
+                <td class="priceGridRow" contenteditable="true">24€</td>
+                <td class="priceGridRow" contenteditable="true">30€</td>
             </tr>
             <tr>
-                <th scope="row">Coiffage</th>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
-                <td contenteditable="true"></td>
+                <th style="padding-left: 5px;" scope="row">Coiffage</th>
+                <td class="priceGridRow" contenteditable="true">16€</td>
+                <td class="priceGridRow" contenteditable="true">20€</td>
+                <td class="priceGridRow" contenteditable="true">24€</td>
+                <td style="padding-left: 5px;" contenteditable="true">30€</td>
             </tr>
         </tbody>
     </table>
